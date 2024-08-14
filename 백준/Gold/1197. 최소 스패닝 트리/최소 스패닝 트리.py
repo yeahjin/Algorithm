d35628f284d9fabf77230a41cdbc1a6e6bfabec2
Yejin <39540603+yeahjin@users.noreply.github.com>
2024-08-15 06:21:18 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 316 ms, Memory: 49332 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,37 @@
+import sys, heapq
+input = sys.stdin.readline
+
+def find(x):
+    global parent
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(x,y):
+    global parent
+    x = find(x)
+    y = find(y)
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+v, e = map(int,input().split())
+parent = []
+for i in range(v+1):
+    parent.append(i)
+
+edges = []
+ans = 0
+
+for _ in range(e):
+    a, b, c = map(int,input().split())
+    heapq.heappush(edges,(c,a,b))
+
+for i in range(e):
+    cost, a, b = heapq.heappop(edges)
+    if find(a) != find(b):
+        union(a,b)
+        ans += cost
+        
+print(ans)
