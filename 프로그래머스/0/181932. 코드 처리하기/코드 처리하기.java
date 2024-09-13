984d4215c7fcbd0d3f29bbe33f1369d256bbc317
Yejin <39540603+yeahjin@users.noreply.github.com>
2024-09-14 04:10:47 +0900

    [level 0] Title: 코드 처리하기, Time: 348.86 ms, Memory: 378 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        boolean mode = true;
+        
+        for (int i =0 ; i < code.length(); i++){
+            if(code.charAt(i) == '1'){
+                mode = !mode;
+                continue;
+            }
+            if(mode && i%2==0){
+                answer += code.charAt(i);
+            }else if(!mode && i%2!=0){
+                answer += code.charAt(i);
+            }
+        }
+        
+        return answer.equals("")? "EMPTY":answer;
+    }
+}
