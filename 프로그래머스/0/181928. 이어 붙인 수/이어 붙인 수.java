dfed3c1d443d7799ef2d91571aacc166f4db7ac9
Yejin <39540603+yeahjin@users.noreply.github.com>
2024-09-14 03:56:40 +0900

    [level 0] Title: 이어 붙인 수, Time: 14.42 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String even = "";
+        String odd = "";
+        
+        for (int i : num_list){
+            if (i%2 == 0){
+                even += i;
+            }else{
+                odd += i;
+            }
+        }
+        return Integer.parseInt(even) + Integer.parseInt(odd);
+    }
+}
