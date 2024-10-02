99925a11818d2d66d5f60abc4ef05a1f88507491
Yejin <39540603+yeahjin@users.noreply.github.com>
2024-10-02 14:15:58 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 322.20 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -0,0 +1,36 @@
+import collections
+def solution(mats, park):
+    answer = -1
+    
+    h = len(park)
+    w = len(park[0])
+    for i in range(len(park)):
+        for j in range(len(park[0])):
+            if park[i][j] != "-1":
+                continue
+            
+            answer = max(mats_sol(mats,park, i, j),answer)
+    
+    
+    return answer
+
+def mats_sol(mats, park, i, j):
+    h = len(park)
+    w = len(park[0])
+    ans = -1
+    for s in mats:
+        flag = False
+        for x in range(s):
+            for y in range(s):
+                ni = x + i
+                nj = y + j
+                if ni < 0 or ni >= h or nj <0 or nj >= w:
+                    flag = True
+                    continue
+                if park[ni][nj] != "-1":
+                    flag = True
+                    continue
+        if flag:
+            continue
+        ans = max(ans, s)
+    return ans
