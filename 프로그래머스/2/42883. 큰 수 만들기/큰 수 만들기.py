d4e3fb20c34b2b76d0288a6de897cac13b0f07ae
Yejin <39540603+yeahjin@users.noreply.github.com>
2025-02-12 15:44:55 +0900

    [level 2] Title: 큰 수 만들기, Time: 97.92 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -2,13 +2,9 @@ def solution(number, k):
     answer = []
     
     for i in number:
-        while k > 0 and answer and answer[-1] < i:
+        while answer and answer[-1] < i and k>0:
             k -= 1
             answer.pop()
         answer.append(i)
     
-    if k > 0:
-        answer = answer[:-k]
-    
-    
-    return "".join(answer)
+    return "".join(answer[:len(number)-k])
