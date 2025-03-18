3d1d4321b9a42a03cd233fbbbd2e984df22183b8
Yejin <39540603+yeahjin@users.noreply.github.com>
2025-03-18 16:51:05 +0900

    [Gold IV] Title: 치즈, Time: 80 ms, Memory: 35004 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -0,0 +1,59 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+dx = [0,0,1,-1]
+dy = [-1,1,0,0]
+
+
+def bfs(y,x):
+    global n,m, on_air
+    q = deque()
+    q.append((y,x))
+    v[y][x] = True
+    ccnt = 0
+    while q:
+        yy, xx = q.popleft()
+        for i in range(4):
+            ny = dy[i] + yy
+            nx = dx[i] + xx
+            if 0 <= ny < n and 0 <= nx < m and not v[ny][nx]:
+                if mm[ny][nx] == 0:
+                    v[ny][nx] = True
+                    q.append((ny,nx))
+                elif mm[ny][nx] == 1 and on_air:
+                    v[ny][nx] = True
+                    mm[ny][nx] = 0
+                    ccnt += 1
+    return ccnt
+
+
+
+n, m = map(int,input().split())
+
+mm = []
+for i in range(n):
+    mm.append(list(map(int,input().split())))
+cnt = 0
+c_cnt = 0
+while True:
+    flag = True
+    for i in range(n):
+        for j in range(m):
+            if mm[i][j] == 1:
+                flag = False
+
+    if flag:
+        break
+
+    on_air = True # 바깥 공기인지 아닌지 판별
+    v = [[False] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if mm[i][j] == 0 and not v[i][j] and on_air:
+                c_cnt = bfs(i,j)
+                on_air = False
+    cnt += 1
+
+print(cnt)
+print(c_cnt)
