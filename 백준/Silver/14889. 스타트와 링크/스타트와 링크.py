30826a5c1f31ed3c7e9a3746216fb83e9b113ce9
Yejin <39540603+yeahjin@users.noreply.github.com>
2025-03-20 01:58:13 +0900

    [Silver I] Title: 스타트와 링크, Time: 6084 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,28 +1,33 @@
-def r(d):
+import sys
+input = sys.stdin.readline
+
+def dfs(d, idx):
     global min_num
-    if d == n:
+    if d == n//2:
         start, link = 0, 0
-        if v.count(1) == n//2:
-            for i in range(n):
-                for j in range(n):
-                    if v[i] == 0 and v[j] == 0:
-                        start += g[i][j]
-                    if v[i] == 1 and v[j] == 1:
-                        link += g[i][j]
-            min_num = min(min_num, abs(start - link))
+        for i in range(n):
+            for j in range(n):
+                if v[i] == 0 and v[j] == 0:
+                    start += g[i][j]
+                if v[i] == 1 and v[j] == 1:
+                    link += g[i][j]
+        min_num = min(abs(start - link), min_num)
         return
-    v[d] = 1
-    r(d + 1)
-    v[d] = 0
-    r( d+1)
+    for i in range(idx,n):
+        if v[i] == 0:
+            v[i] = 1
+            dfs(d+1,i+1)
+            v[i] = 0
+
 
 
 n = int(input())
-v = [0] * n
 g = []
+
 for i in range(n):
     t = list(map(int,input().split()))
     g.append(t)
+v = [0] * n
 min_num = int(1e9)
-r(0)
+dfs(0,0)
 print(min_num)
