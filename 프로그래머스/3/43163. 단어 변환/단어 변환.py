1932bd34d9cc0444e3ada8988def2066b0a0acc5
Yejin <39540603+yeahjin@users.noreply.github.com>
2025-03-27 05:21:39 +0900

    [level 3] Title: 단어 변환, Time: 7.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,30 +1,37 @@
 from collections import deque
-def compare(x,y):
-    cnt = 0
-    for i in range(len(x)):
-        if x[i] != y[i]:
-            cnt += 1
-    if cnt == 1:
+
+def valid(a,b):
+    tmp = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            tmp += 1
+    if tmp == 1:
         return True
     else:
         return False
 
-def bfs(begin,words,target):
+
+def bfs(begin, target, words):
     q = deque()
-    
-    q.append((begin,0))
-    
+    for i in words:
+        if valid(begin, i):
+            q.append((i, 1))
     while q:
         word, cnt = q.popleft()
         if word == target:
             return cnt
         for i in words:
-            if compare(i, word):
+            if valid(word,i):
                 q.append((i, cnt + 1))
     
 
+
 def solution(begin, target, words):
+    answer = 0
+    
     if target not in words:
         return 0
-    answer = bfs(begin,words,target)
+    
+    answer = bfs(begin, target, words)
+    
     return answer
