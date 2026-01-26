00aef18d58e069a83707aefaca06c548dd41826d
Yejin <39540603+yeahjin@users.noreply.github.com>
2026-01-27 00:00:13 +0900

    [Gold V] Title: 암호 만들기, Time: 116 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,48 +1,45 @@
 import java.util.*;
+
 public class Main {
-	static boolean[] visited;
-	static String[] arr;
-	static int l;
-	static ArrayList<String> arr2 = new ArrayList<String>();
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		l = sc.nextInt();
-		int c = sc.nextInt();
-		visited = new boolean[c];
-		arr = new String[c];
-		for (int i = 0; i < c; i++) {
-			arr[i] = sc.next();
-		}
-		Arrays.sort(arr);
-		
-		combination(0,l);
-		for (int i = arr2.size()-1; i >=0; i--) {
-			System.out.println(arr2.get(i));
-		}
-	}
-	private static void combination(int start, int r) {
-		if(r==arr.length) {
-			String tmp = new String();
-			int cnt = 0;
-			for (int i = 0; i < arr.length; i++) {
-				if(!visited[i]) {
-					if(arr[i].equals("a")||arr[i].equals("e")||arr[i].equals("i")||arr[i].equals("o")||arr[i].equals("u")) {
-						cnt++;
-					}
-					tmp += arr[i];
-					
-				}
-			}
-			if(cnt >= 1 && l - cnt >= 2) {
-				arr2.add(tmp);
-			}
-			return;
-		}
-		for (int i = start; i < arr.length; i++) {
-			visited[i] = true;
-			combination(i+1, r+1);
-			visited[i] = false;
-		}
-	}
+    static int L, C;
+    static char[] list;
+    static char[] picked;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        L = sc.nextInt();
+        C = sc.nextInt();
+        list = new char[C];
+        picked = new char[L];
+        for (int i = 0; i < C; i++) list[i] = sc.next().charAt(0);
+
+        Arrays.sort(list);
+        combinations(0,0);
+
+    }
+    public static void combinations(int dep, int start){
+        if (dep == L){
+            int vowel = 0;
+            int constant = 0;
+            for (int i = 0; i < L; i++){
+                if (picked[i] == 'a' || picked[i] == 'e' || picked[i] == 'i' || picked[i] == 'o' || picked[i] == 'u'){
+                    vowel++;
+                } else{
+                    constant++;
+                }
+            }
+
+            if (vowel >= 1 && constant >= 2){
+                for (int i = 0; i < L; i++){
+                    System.out.print(picked[i]);
+                }
+                System.out.println();
+            }
+            return;
+        }
+        for (int i = start; i < C; i++){
+            picked[dep] = list[i];
+            combinations(dep + 1, i + 1);
+        }
+    }
 
 }
