c25a75455efe08cc001783a720f0434f83a3965d
Yejin <39540603+yeahjin@users.noreply.github.com>
2026-03-18 22:38:58 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 15044 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0 ; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        double max = arr[n-1];
+        double new_score = 0.0;
+        for (int i = 0 ; i < n; i++){
+            new_score += arr[i] / max * 100;
+        }
+        System.out.println(new_score/n);
+    }
+}
